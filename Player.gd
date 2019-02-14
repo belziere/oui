fa4d6afcb98c3343a1edb26e42e8103877fb03e5
belziere <bastienelziere@gmail.com>
2019-02-14 14:39:53 +0100

    Add files via upload

---
+++ Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+signal BOOP
+
+export (int) var speed 
+var screensize
+
+func _ready():
+	screensize = get_viewport_rect().size
+	
+func _physics_process(delta):
+	move_and_collide(Vector2(0, 0))
+	var velocity = Vector2() 
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		position += velocity * delta
+		position.x = clamp(position.x, 0, screensize.x)
+		position.y = clamp(position.y, 0, screensize.y)
+	if Input.is_action_just_pressed("ui_accept"):
+		emit_signal("BOOP")
---
+++ map.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+# Internal node references
+onready var TileMap = $TileMap
+onready var Player = $TileMap/Player
+onready var Mob = $TileMap/Mob
+
+
+# Performed when added to scene
+func _ready():
+
+	# Connects the whistle to creating a new path
+	Player.connect("BOOP", self, "_calculate_new_path")
+
+
+# Calculates a new path and gives to sidekick
+func _calculate_new_path():
+
+	# Finds path
+	var path = TileMap.get_path(Mob.position, Player.position)
+
+	# If we got a path...
+	if path:
+		
+		# Remove the first point (it's where the sidekick is)
+		path.remove(0)
+		
+		# Sets the sidekick's path
+		Mob.path = path
