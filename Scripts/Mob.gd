3b7e0a6886a37f272b17ef47a34227312e30b661
bastien <bastienelziere@gmail.com>
2019-02-14 20:16:58 +0100

    oui

--- Scripts/Mob.gd
+++ Scripts/Mob.gd
@@ -1,11 +1,11 @@
 extends KinematicBody2D
-onready var Player = get_root("res://Scenes/map.tscn/")
+#onready var Player = get_root("res://Scenes/map.tscn/")
 
 func _ready():
 	pass
 
-func _process(delta):
+#func _process(delta):
 	
-	var PlayerPos = Player.get_global_position()
-	var selfPos = get_position()
+#	var PlayerPos = Player.get_global_position()
+#	var selfPos = get_position()
 	
